c679dc188aa9034c2e0e40710dd678521d63e9ba
Alexander Salois <asalois.scholar@gmail.com>
2021-07-08 09:25:48 -0600

    fixed making docker

--- run_docker_gpu.sh
+++ run_docker_gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # script to organize the output and make new dir
 #docker run -it  -v /home/alexandersalois/Documents/tf_docker:/results tf /bin/bash
-docker run -d  -v /home/alexandersalois/Documents/deepNNEq:/code tfgpu /bin/bash run_deep.sh 39
+docker run --runtime=nvidia --privileged -d  -v /home/alexandersalois/Documents/deepNNEq:/code tfgpu /bin/bash run_deep.sh 39
 
