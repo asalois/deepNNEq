6448fc943034654b71cd28d4278731b4e3a28e3b
Alex Salois <asalois.scholar@gmail.com>
2021-07-13 11:37:08 -0600

    Dockerfile adjustments

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,12 @@ FROM tensorflow/tensorflow
 RUN apt-get update && apt-get install -y \
     python3-pip \
     vim && \
-    pip3 install scipy
+    pip3 install scipy boto3 &&\
+    mkdir /code
+
+COPY deep_nnEq.py /code
+
+COPY run_deep.sh /code
 
 WORKDIR /code/ 
 
--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # script to organize the output and make new dir
-#docker run -it  -v /home/alexandersalois/Documents/tf_docker:/results tf /bin/bash
-docker run -d  -v /home/alexandersalois/Documents/deepNNEq:/code tf /bin/bash run_deep.sh 40
+#docker run -it  -v /home/alexwsl/deepNNEq:/code tf /bin/bash
+#docker run -d  -v /home/alexandersalois/Documents/deepNNEq:/code tf /bin/bash run_deep.sh 40
+docker run -it  tf /bin/bash
 
