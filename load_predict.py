12ec3d447107fb14363b4e8b4b488f1246df6764
Alexander Salois <asalois.scholar@gmail.com>
2021-07-14 11:38:45 -0600

    now just load and print model params

--- load_predict.py
+++ load_predict.py
@@ -4,19 +4,23 @@ import sys
 import os
 import scipy.io as spio
 import math
+import time
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.losses import MeanSquaredError
+start_time = time.time()
+
+
 SNR = str(sys.argv[1])
 print(SNR)
 
-model = keras.models.load_model('deep_model_SNR40.h5')
+SNR = str(sys.argv[1])
+SNRs = str(SNR).zfill(2)
+print(SNRs)
+savename = "deep_model_SNR" + SNRs + '.h5'
+model = keras.models.load_model(savename)
 model.summary()
 
-
-# Load the data
-matname = "predictionsSNR" + SNR + ".mat"
-print(matname)
-mat = spio.loadmat(matname, squeeze_me=True)
+print("--- %.2f seconds ---" % (time.time() - start_time))
